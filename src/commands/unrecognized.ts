e059d63d23dcec60a31884c485e5812208621486
Caleb Chan <ccss009013@gmail.com>
2020-03-27 17:50:35 -0400

    serverConfig/Queue => config/queue.get

--- src/commands/unrecognized.ts
+++ src/commands/unrecognized.ts
@@ -1,7 +1,7 @@
 import { CommandArguments } from './shared/args';
-import { messageDetails } from './shared/messageDetails';
+import { sendCurrentState } from './shared/sendCurrentState';
 
-export function unrecognized({ msg, args, cmd, serverPrefix }: CommandArguments) {
-  return msg.channel.send(`Unrecognized command \`${cmd}\`.
-${messageDetails({ msg, cmd, args, serverPrefix })}`);
+export async function unrecognized({ msg, args, cmd, queue, config }: CommandArguments) {
+  await msg.channel.send(`Unrecognized command \`${cmd}\`.`);
+  sendCurrentState({ msg, args, cmd, config, queue });
 }
