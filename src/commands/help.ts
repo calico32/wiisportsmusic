a2c3cbf336ef0018f13ed5a36a65550595583285
Caleb Chan <ccss009013@gmail.com>
2020-03-27 17:44:43 -0400

    help command

---
+++ src/commands/help.ts
@@ -0,0 +1,49 @@
+import { MessageEmbed } from 'discord.js';
+import * as path from 'path';
+import { CommandArguments } from './shared/args';
+import { createFields, embedDefaults, embedSpacer } from './shared/embed';
+import { sendCurrentState } from './shared/sendCurrentState';
+
+export async function help({ msg, cmd, args, config, queue }: CommandArguments) {
+  const { prefix } = config.get(msg.guild.id);
+  const command = name => `\`${prefix}${name}\``;
+
+  const embed = new MessageEmbed()
+    .setAuthor(...embedDefaults.author)
+    .setColor(embedDefaults.color)
+    .setTitle('Help')
+    .addFields(
+      createFields(
+        [
+          'Global Commands', 
+          'These commands can be used in any channel.', 
+          false
+        ],
+        ['Ping', command('ping')],
+        ['Set bot prefix', command('prefix')],
+        ['Initial setup', command('install')],
+        ['Remove configuration', command('uninstall')],
+        ['Help menu', command('help')],
+        embedSpacer,
+        [ 
+          'Music Channel Commands',
+          'These commands can only be used in the control channel. Prefixes and extra parameters are optional.',
+          false
+        ],
+        ['Lookup + play', command('play')],
+        ['Search videos', command('search')],
+        ['Play video/playlist', '<YouTube URL> --loops <number>'],
+        ['Help menu', command('help')]
+      )
+    );
+  await msg.channel.send({
+    embed,
+    files: [
+      {
+        attachment: path.resolve('./assets/circle-192.png'),
+        name: 'circle-192.png'
+      }
+    ]
+  });
+  sendCurrentState({ msg, args, cmd, config, queue });
+}
