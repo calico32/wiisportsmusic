2d4c6740135539a674727d36c9d82360aff9e95b
Caleb Chan <ccss009013@gmail.com>
2020-03-27 17:46:32 -0400

    youtube, config/queue typings

--- src/commands/shared/args.ts
+++ src/commands/shared/args.ts
@@ -1,10 +1,52 @@
-import { Message, PartialMessage, Snowflake } from 'discord.js';
+import * as Discord from 'discord.js';
+import YoutubeAPI from 'simple-youtube-api';
+import { Video } from './youtube';
 
 export interface CommandArguments {
-  msg: Message | PartialMessage;
-  args: Array<string>;
-  cmd: string;
-  serverPrefix?: string;
-  prefixes?: Map<Snowflake, string>;
-  prefixesFilePath?: string;
+  msg: Discord.Message | Discord.PartialMessage;
+  args?: Array<string>;
+  cmd?: string;
+  config?: ConfigMap;
+  queue?: QueueMap;
+  youtube?: YoutubeAPI;
+  video?: Video;
+  voiceChannel?: Discord.VoiceChannel;
+}
+export type ConfigMap = Map<Discord.Snowflake, GuildConfig>;
+export type QueueMap = Map<Discord.Snowflake, GuildQueue>;
+
+export interface GuildConfig {
+  prefix: string;
+  controlChannelId: Discord.Snowflake;
+  setupCompleted: boolean;
+}
+
+export interface GuildQueue {
+  playing: boolean;
+  voiceChannel: Discord.VoiceChannel;
+  connection: Discord.VoiceConnection;
+  songs: Array<Song>;
+  volume: 0|1|2|3|4|5;
+}
+
+export const defaults = {
+  config: <GuildConfig>{
+    prefix: '^',
+    setupCompleted: false,
+  },
+  queue: <GuildQueue>{
+    songs: [],
+    volume: 3,
+    playing: false,
+  }
+};
+
+export interface Song {
+  video: {
+    id: string;
+    title: string;
+    url: string;
+    
+  }
+  requester: Discord.User;
 }
---
+++ src/commands/shared/youtube.ts
@@ -0,0 +1,91 @@
+import YoutubeAPI from 'simple-youtube-api';
+
+export interface Channel {
+  commentCount: number;
+  country: string;
+  customURL: string;
+  defaultLanguage: string;
+  description: string;
+  full: boolean;
+  hiddenSubscriberCount: boolean;
+  id: string;
+  kind: string;
+  localized: Object;
+  publishedAt: Date;
+  raw: Object;
+  relatedPlaylists: Object;
+  subscriberCount: number;
+  thumbnails: Object;
+  title: string;
+  type: string;
+  url: string;
+  videoCount: number;
+  viewCount: number;
+  youtube: YoutubeAPI;
+  extractID: (urL: string) => string;
+  fetch: (options?: object) => Channel;
+
+}
+export interface Video {
+  channel: Channel;
+  description: string;
+  duration: DurationObject;
+  durationSeconds: number;
+  full: boolean;
+  id: string;
+  kind: string;
+  maxRes: object;
+  publishedAt: Date;
+  raw: object;
+  shortURL: string;
+  thumbnails;
+  title: string;
+  url: string;
+  youtube: YoutubeAPI;
+  extractID: (url: string) => string;
+  fetch: (options?: object) => Promise<Video>;
+}
+export interface Playlist {
+  channel: Channel;
+  channelTitle: string;
+  defaultLanguage: string;
+  description: string;
+  embedHTML: string;
+  id: string;
+  length: number;
+  localized: Object;
+  privacy: string;
+  publishedAt: Date;
+  thumbnails;
+  title: string;
+  url: string;
+  videos: Array<Video>
+  youtube: YoutubeAPI;
+  fetch: (options?: object) => Video;
+}
+export interface Youtube {
+  key: string;
+  getChannel: (url: string, options?: object) => Promise<Channel>;
+  getChannelByID: (id: string, options?: object) => Promise<Channel>;
+  
+  getPlaylist: (url: string, options?: object) => Promise<Playlist>;
+  getPlaylistByID: (id: string, options?: object) => Promise<Playlist>;
+  
+  getVideo: (url: string, options?: object) => Promise<Video>;
+  getVideoByID: (id: string, options?: object) => Promise<Video>;
+
+  search: (query: string, limit?: number, options?: object) => Promise<Array<(Video|Playlist|Channel|null)>>
+  
+  searchChannels: (query: string, limit?: number, options?: object) => Promise<Array<Channel>>
+  
+  searchPlaylists: (query: string, limit?: number, options?: object) => Promise<Array<Playlist>>
+  
+  searchVideos: (query: string, limit?: number, options?: object) => Promise<Array<Video>>
+  
+}
+
+export type DurationObject = {
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
