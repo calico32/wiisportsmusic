73ea1e39b004d6e04ad3ade2a7f2854b47d3eb5f
Caleb Chan <ccss009013@gmail.com>
2020-03-27 17:46:54 -0400

    mapToJSON indentation

--- src/Util.ts
+++ src/Util.ts
@@ -1,8 +1,9 @@
 export default class Util {
   static jsonToMap = (jsonString: string): Map<any, any> => {
     return new Map(JSON.parse(jsonString));
-  }
-  static mapToJSON = (map: Map<any, any>): string => {
-    return JSON.stringify([...map]);
-  }
+  };
+  static mapToJSON = (map: Map<any, any>, indent?: boolean): string => {
+    if (indent) return JSON.stringify([...map], null, 2);
+    else return JSON.stringify([...map]);
+  };
 }
